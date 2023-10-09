aba83b8c3e2481e3b1409f2ea37d87a7d7053d57
Ambar <brannstrom9911@gmail.com>
2023-10-09 21:23:30 +0530

    Initial Commit

---
+++ CachiPy/__init__.py
@@ -0,0 +1,1 @@
+from .CachiPy import cacheit
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A caching library specially designed for FastAPI'
+with open('README.md', 'r', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="CachiPy",
+    version=VERSION,
+    author="Ambar Rizvi",
+    author_email="<brannstrom9911@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    py_modules=["CachiPy"],
+    package_dir={'' : 'CachiPy'},
+    install_requires=['requests==2.31.0', 'typing==3.7.4.3'],
+    keywords=['cache', 'caching', 'API response', 'API caching', 'FastAPI cache', 'FastAPI caching', 'fastapi cacher'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
